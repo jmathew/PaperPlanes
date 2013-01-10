a2567e53cbf74cd2451ae03a7181b7a426a2c9d8
Jeff Mathew <jm8thew@gmail.com>
2013-01-09 19:32:46 -0500

    Added some simple box2d integration attempts.

--- main/src/com/jeff/paperplanes/MainScreen.java
+++ main/src/com/jeff/paperplanes/MainScreen.java
@@ -11,8 +11,20 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Vector2;
 
 import com.badlogic.gdx.Screen;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+
+
 
 public class MainScreen implements Screen {
 
@@ -22,6 +34,14 @@ public class MainScreen implements Screen {
   SpriteBatch spb;
   Vector3 touchPos;
   OrthographicCamera cam;
+
+  World theWorld;
+  BodyDef planeBodyDef;
+  Body planeBody;
+  FixtureDef planeFixDef;
+  Fixture planeFix;
+ 
+  PolygonShape planeShape; 
 
   public MainScreen( PaperPlanesGame g ) {
     this.g = g;
@@ -43,6 +63,28 @@ public class MainScreen implements Screen {
     // touch location
     touchPos = new Vector3();
 
+    // box2d 
+    theWorld = new World( new Vector2( 1f, 1f ), true );
+
+    planeBodyDef = new BodyDef();
+    planeBodyDef.type = BodyType.DynamicBody;
+    planeBodyDef.position.set( new Vector2( 0f, 0f ) );
+
+    planeBody = theWorld.createBody( planeBodyDef );
+
+    planeShape = new PolygonShape();
+    planeShape.setAsBox( planeImage.getWidth()/2, planeImage.getHeight()/2 ); //this might be in meters
+    
+    planeFixDef = new FixtureDef();
+    planeFixDef.shape = planeShape;
+    planeFixDef.density = 1.0f;
+    planeFixDef.friction = 0.0f;
+    planeFixDef.restitution = 1;
+  
+    planeBody.createFixture( planeFixDef );
+
+    planeShape.dispose();
+
   }
   @Override
   public void show() {
@@ -55,15 +97,21 @@ public class MainScreen implements Screen {
     // update camera
     cam.update();
 
-    
+    // get info from our physics body
+    Vector2 t = new Vector2();
+    t = planeBody.getPosition();
+    Gdx.app.log( "X + Y", t.x + " + " + t.y );
+
     // begin draw
     spb.setProjectionMatrix( cam.combined );
     spb.begin();
 
     // move our plane and center it
-    spb.draw( planeImage, planeRect.x - ( planeImage.getWidth() / 2 ) , planeRect.y - ( planeImage.getHeight() / 2 ) );
+    //spb.draw( planeImage, planeRect.x - ( planeImage.getWidth() / 2 ) , planeRect.y - ( planeImage.getHeight() / 2 ) );
     //spb.draw( planeImage, planeRect.x, planeRect.y );
+    spb.draw( planeImage, t.x * 50, t.y * 50 );
     spb.end();
+
 
     // update touch position
     if( Gdx.input.isTouched() ) {
@@ -76,8 +124,11 @@ public class MainScreen implements Screen {
       planeRect.x = touchPos.x;
       planeRect.y = touchPos.y;
     }
-    
-    Gdx.app.log( "X + Y", planeRect.x + " + " + planeRect.y );
+
+
+    //Gdx.app.log( "X + Y", planeRect.x + " + " + planeRect.y );
+
+    theWorld.step( 1/60f, 6, 2 );
 
   }
 
